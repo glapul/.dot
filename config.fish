db86634d8d9ae3655eaea2bc840314bb82784b7b
Michał Glapa <michal.glapa@gmail.com>
2015-05-25 22:02:16 +0200

    minor updates

--- config.fish
+++ config.fish
@@ -1,2 +1,2 @@
-set PATH $PATH /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin /home/glapul/bin /usr/local/texlive/2014/bin/x86_64-linux/ /home/glapul/bin/sublime_text_2/
+set PATH $PATH /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin /home/glapul/bin /usr/local/texlive/2014/bin/x86_64-linux/ /home/glapul/bin/sublime_text_2/ /home/glapul/eclipse/
 set CXXFLAGS -ggdb -std=c++11 -Wall -Wformat
